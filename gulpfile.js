445daabc60f71fb4ec4f5d1210dba4ea20a2ca6c
Myke Bates <mbates@thealchemediaproject.com>
2014-11-05 13:42:24 -0600

    slimming down for preso

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,14 @@
-var gulp = require('gulp'),
-	watch = require('gulp-watch'),
-	livereload = require('gulp-livereload'),
-	sass = require('gulp-sass'),
-	//sass = require('gulp-ruby-sass'),
-	minifyCSS = require('gulp-minify-css'),
-	plumber = require('gulp-plumber');
+var gulp = require('gulp');
 
-gulp.task('sass-simple', function () {
-	gulp.src('./scss/main.scss')
-		.pipe(plumber())
-		//.pipe(sass())
-		.pipe(sass({
-			'sourcemap': true,
-			'sourcemapPath': '../scss',
-			'style': 'compressed'
-		}))
-		.pipe(minifyCSS({keepSpecialComments:false}))
-		.pipe(gulp.dest('./css'))
-		.pipe(livereload());
-});
+//gulp.task('sass', function () {
+//	gulp.src('./scss/main.scss')
+//		.pipe(sass())
+//		.pipe(gulp.dest('./css'));
+//});
 
-gulp.task('watch', function(){
-	gulp.watch('./scss/**/*.scss', ['sass-simple']);
-});
+//gulp.task('watch', function(){
+//	gulp.watch('./scss/**/*.scss', ['sass']);
+//});
 
-gulp.task('default', function () {
-	console.log('hello gulp!');
-});
+
+// Default Gulp Task
